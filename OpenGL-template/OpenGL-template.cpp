3fabd9e2bcb29ff5ab00e2d58c02190b7633d47a
user <user@DESKTOP-ON6JLGM>
2025-05-03 11:50:58 +0200

    Add project files.

---
+++ OpenGL-template/OpenGL-template.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL-template.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGL-template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
